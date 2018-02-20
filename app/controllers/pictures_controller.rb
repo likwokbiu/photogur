38f0de64a67c1a29bc11178a15d47ed8fd7d2f44
likwokbiu <li_bill@ymail.com>
2018-02-20 15:43:01 -0500

    First Rails app done.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+
   def index
     @pictures = Picture.all
   end
@@ -26,5 +27,30 @@ class PicturesController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    @picture.title = params[:picture][:title]
+    @picture.artist = params[:picture][:artist]
+    @picture.url = params[:picture][:url]
+
+
+    if @picture.save
+      redirect_to "/pictures/#{@picture.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to "/pictures"
+  end  
 
 end
